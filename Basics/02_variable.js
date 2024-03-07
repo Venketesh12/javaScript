7d11e15205427763e39ce5863f1a26c3d766eb3f
K.Venketesh Dora <kvenketeshdora@gmail.com>
2024-03-08 03:26:30 +0530

    rename file name

---
+++ Basics/02_variable.js
@@ -0,0 +1,8 @@
+const accountId = 12345
+let accountEmail = "kvenketeshdora@gmail.com"
+var accountPassword = "0008"
+accountCity = "Bengaluru"
+
+// accountId = 2
+accountEmail = "kvd@gmail.com"
+console.table([accountEmail, accountId, accountPassword, accountCity])
